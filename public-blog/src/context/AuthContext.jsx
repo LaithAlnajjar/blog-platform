ba8c506446efd4e9c27c1d6eff98f40bdb95bef9
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-03-22 15:59:28 +0300

    feat: Implement logging in

---
+++ public-blog/src/context/AuthContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const storedUser = localStorage.getItem('user');
+  const storedToken = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (storedUser && storedToken) {
+      setUser(JSON.parse(storedUser));
+      setToken(storedToken);
+    }
+  }, [storedUser, storedToken]);
+
+  const login = async (data) => {
+    try {
+      const { username, password } = data;
+      const response = await axios.post('http://localhost:3000/login', {
+        username,
+        password,
+      });
+      if (response.data.success) {
+        setUser(response.data.data.user);
+        setToken(response.data.data.token);
+        localStorage.setItem('user', JSON.stringify(response.data.data.user));
+        localStorage.setItem('token', response.data.data.token);
+        return;
+      }
+      throw new Error(response.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
--- public-blog/src/pages/Login.jsx
+++ public-blog/src/pages/Login.jsx
@@ -1,15 +1,63 @@
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import styles from './Login.module.css';
+
 function Login() {
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+  });
+
+  const auth = useAuth();
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    try {
+      e.preventDefault();
+      auth.login(input);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <form action="http://localhost:3000/login" method="POST"></form>
+    <form
+      action="http://localhost:3000/login"
+      method="POST"
+      onSubmit={handleSubmit}
+      className={styles['form']}
+    >
+      <h2 className={styles['title']}>Login</h2>
       <label htmlFor="username">
-        <input type="text" id="username" name="username" />
+        Username
+        <input
+          type="text"
+          id="username"
+          name="username"
+          onChange={handleChange}
+          className={styles['username'] + ' ' + styles['input']}
+        />
       </label>
       <label htmlFor="password">
-        <input type="password" id="password" name="password" />
+        Password
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          className={styles['password'] + ' ' + styles['input']}
+        />
       </label>
-    </div>
+      <button type="submit" className={styles['button']}>
+        Login
+      </button>
+    </form>
   );
 }
 
