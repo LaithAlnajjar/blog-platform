7491a88720b17b4c7ee9db48a52c1b92f774b9bb
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-03-23 17:30:36 +0300

    feat: Showcase posts on homepage

--- public-blog/src/components/PostCard.jsx
+++ public-blog/src/components/PostCard.jsx
@@ -1,3 +1,10 @@
-function PostCard() {}
+function PostCard({ title, content }) {
+  return (
+    <div className="post-card">
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+}
 
 export default PostCard;
--- public-blog/src/pages/Home.jsx
+++ public-blog/src/pages/Home.jsx
@@ -1,3 +1,25 @@
-function Home() {}
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import PostCard from '../components/PostCard';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await axios.get('http://localhost:3000/posts');
+      setPosts(response.data.data);
+      console.log(response.data.data);
+    };
+    getPosts();
+  }, []);
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostCard key={post.id} title={post.title} content={post.content} />
+      ))}
+    </div>
+  );
+}
 
 export default Home;
