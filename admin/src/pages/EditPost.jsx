7bb38f1c0ae3443fce10deb49cd094110e3e26da
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-03-28 15:33:56 +0300

    feat: Implement post editing functionality

--- admin/src/pages/EditPost.jsx
+++ admin/src/pages/EditPost.jsx
@@ -1,5 +1,60 @@
-function EditPost() {
-  return <div></div>;
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Editor } from '@tinymce/tinymce-react';
+
+function PostEdit() {
+  const { id } = useParams();
+  const [post, setPost] = useState({ title: '', content: '' });
+  const navigate = useNavigate();
+  const apiKey = import.meta.env.VITE_TINY_MCE_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/posts/${id}`)
+      .then((res) => setPost(res.data.data));
+  }, [id]);
+
+  const handleEditorChange = (content) => {
+    setPost({ ...post, content });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(
+        `http://localhost:3000/posts/${id}`,
+        { ...post },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      );
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Error updating post:', error);
+    }
+    navigate('/posts');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Edit Post</h2>
+      <input
+        type="text"
+        name="title"
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+      />
+      <Editor
+        apiKey={apiKey}
+        value={post.content}
+        onEditorChange={handleEditorChange}
+      />
+      <button type="submit">Save Changes</button>
+    </form>
+  );
 }
 
-export default EditPost;
+export default PostEdit;
