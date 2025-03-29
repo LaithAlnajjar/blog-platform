f6ce72ed1b9884f17dd766495505d09c794e6777
Laith Alnajjar <laithalnajjar2004@gmail.com>
2025-03-29 04:21:40 +0300

    feat: Create Footer component

--- public-blog/src/components/Footer.jsx
+++ public-blog/src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import styles from '../../styles/Footer.module.css';
+
 function Footer() {
   return (
-    <footer>
-      <p>Created by Laith Alnajjar</p>
+    <footer className={styles['footer']}>
+      <div className={styles['container']}>
+        <p className={styles['text']}>
+          © {new Date().getFullYear()} Code & Craft. All rights reserved.
+        </p>
+        <p className={styles['credit']}>
+          Created by{' '}
+          <a
+            href="https://github.com/laithalnajjar"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Laith Alnajjar
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
